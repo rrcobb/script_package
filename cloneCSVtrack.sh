9dcf9f96909ab0d74ddd90c566f8b655fa830493
Robert W Cobb <robert.cobb@flatironschool.com>
2019-04-24 15:13:16 -0400

    use ssh instead of https
    to avoid github password prompt

--- cloneCSVtrack.sh
+++ cloneCSVtrack.sh
@@ -1,3 +1,10 @@
-mkdir $1
-cd $1
-cat ../$1.csv | while IFS="," read -r a b c d e; do temp="${e%\"}"; temp="${temp#\"}"; git clone "$temp"; done
+## turn a github link a la https://github.com/learn-co-curriculum/What-To-Expect-In-Mod-3
+# into a ssh link, git@github.com:learn-co-curriculum/What-To-Expect-In-Mod-3.git
+function github-link-from-https-to-ssh() {
+  temp="$(echo $1 | sed 's+https://github.com/+git@github.com:+g')"
+  echo "$temp.git"
+}
+
+mkdir $2
+cd $2
+cat $1 | while IFS="," read -r a b c d e; do temp="${e%\"}"; temp="${temp#\"}"; temp="$(github-link-from-https-to-ssh $temp)";
